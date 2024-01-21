1ca2e49b7d3d81094630d44d51429d70cae33cc4
Yuto Mabe <yuto96315mabe@gmail.com>
2024-01-21 16:06:23 +0900

    logの内容を修正

--- src/functions/auth.ts
+++ src/functions/auth.ts
@@ -1,12 +1,14 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./firebase";
 
+
+// authにユーザー登録
 export const signUpWithEmail = async (email: string, password: string) => {
     // パスワードが6文字以下の場合にはエラーをスローする
     try {
         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
         const user = userCredential.user;
-        console.log(`サインアップが完了しました。${user.displayName}`);
+        console.log(`サインアップが完了しました。${user.uid}`);
         return user;
     } catch (error) {
         console.log(`サインアップでエラーが発生しました。${error}`);
